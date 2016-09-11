ab19f62447959bbc1d5b743e8142fa2e75bed58e
Paulo Ragonha <paulo@ragonha.me>
2016-09-11 23:58:00 +0200

    Add a search box and selection state 😂

--- ui/src/components/note-list.js
+++ ui/src/components/note-list.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import {List, ListItem} from 'material-ui/List'
+import {List, ListItem, MakeSelectable} from 'material-ui/List'
 import Divider from 'material-ui/Divider'
 import Subheader from 'material-ui/Subheader'
 import { darkBlack } from 'material-ui/styles/colors'
+
+import Menu from 'material-ui/Menu'
+import MenuItem from 'material-ui/MenuItem'
+import Search from 'material-ui/svg-icons/action/search'
+import TextField from 'material-ui/TextField'
+
+const SelectableList = MakeSelectable(List)
 
 export default React.createClass({
   render () {
     return (
       <div>
-        <List>
+        <Menu>
+          <MenuItem leftIcon={<Search />}>
+            <TextField hintText='Search' fullWidth />
+          </MenuItem>
+        </Menu>
+
+        <SelectableList value={1}>
           <Subheader>Notes</Subheader>
           <ListItem
+            value={0}
             primaryText='My first note'
             secondaryText={
               <p>
@@ -22,6 +36,7 @@ export default React.createClass({
           />
           <Divider />
           <ListItem
+            value={1}
             primaryText='My second note'
             secondaryText={
               <p>
@@ -32,7 +47,7 @@ export default React.createClass({
             secondaryTextLines={2}
           />
           <Divider />
-        </List>
+        </SelectableList>
       </div>
     )
   }
